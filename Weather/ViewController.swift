672a7a4b3530f42279e8320e00694704dedd8032
BorodinArtemX <BorodinArtemX@icloud.com>
2023-03-09 14:25:42 +0100

    Complete App

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var getWeatherButtonTap: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getWeatherButtonTap.addTarget(self, action: #selector(didTapGetWeather), for: .touchUpInside)
     }
-
-
+    
+    @objc func didTapGetWeather() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                self.weatherLabel.text = "\(weather.currentWeather.temperature) °"
+            }
+        } else {
+            print("Fail!")
+        }
+    }
+        task.resume()
+}
 }
 
---
+++ Weather/WeatherData.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherCome.swift
+//  Weather
+//
+//  Created by Артем Бородин on 05/03/2023.
+//
+
+import Foundation
+
+// MARK: - Empty
+struct WeatherData: Codable {
+    let latitude, longitude, generationtimeMS: Double
+    let utcOffsetSeconds: Int
+    let timezone, timezoneAbbreviation: String
+    let elevation: Int
+    let currentWeather: CurrentWeather
+
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude
+        case generationtimeMS = "generationtime_ms"
+        case utcOffsetSeconds = "utc_offset_seconds"
+        case timezone
+        case timezoneAbbreviation = "timezone_abbreviation"
+        case elevation
+        case currentWeather = "current_weather"
+    }
+}
+
+// MARK: - CurrentWeather
+struct CurrentWeather: Codable {
+    let temperature, windspeed: Double
+    let winddirection, weathercode: Int
+    let time: String
+}
